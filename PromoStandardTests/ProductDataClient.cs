e90f844c304c053b9d66d10566b67f9795ee841e
eleon <eleonzx@gmail.com>
2017-07-31 23:46:02 -0400

    initial commit

---
+++ PromoStandardTests/ProductDataClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PricingAndConfigurationTest
+{
+    class ProductDataClient
+    {
+        private WSProductData.ProductDataServiceClient wsClient;
+        public List<Type> validTypes;
+        public string path;
+
+
+        public ProductDataClient()
+        {
+            wsClient = new WSProductData.ProductDataServiceClient();
+            var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+                    where t.IsClass && t.Namespace == "PricingAndConfigurationTest.WSProductData"
+                    select t;
+            validTypes = q.ToList();
+        }
+
+
+        public WSProductData.GetProductResponse getProductData()
+        {
+            this.setPath("productDataOutput");
+            var request = new WSProductData.GetProductRequest();
+            request.wsVersion = "1.0.0";
+            request.id = ConfigurationManager.AppSettings.Get("wsUserName");
+            request.password = ConfigurationManager.AppSettings.Get("wsPassword");
+            request.localizationCountry = "US";
+            request.localizationLanguage = "en";
+            request.productId = "1035";
+            var request1 = new WSProductData.getProductRequest1(request);
+            WSProductData.getProductResponse1 response1 = wsClient.getProduct(request1);
+            return response1.GetProductResponse;
+        }
+
+        public void setPath(string path)
+        {
+            this.path = Environment.ExpandEnvironmentVariables(Path.Combine(ConfigurationManager.AppSettings.Get("responseSavePath"), path + ".txt"));
+            File.Delete(path);
+        }
+    }
+}
---
+++ PromoStandardTests/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PricingAndConfigurationTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            PricingAndConfigurationClient pricingAndConfigurationTester = new PricingAndConfigurationClient();
+
+            //getFobPoints test
+            var fobPointsResponse = pricingAndConfigurationTester.getFobPoints();
+            printResponse(fobPointsResponse, pricingAndConfigurationTester.path, pricingAndConfigurationTester.validTypes);
+
+            //getConfigurationAndPricing test
+            var configurationAndPricesResponse = pricingAndConfigurationTester.getConfigurationAndPricing();
+            printResponse(configurationAndPricesResponse, pricingAndConfigurationTester.path, pricingAndConfigurationTester.validTypes);
+
+            //getProductData test
+            var productDataTester = new ProductDataClient();
+            var productDataResponse = productDataTester.getProductData();
+            printResponse(productDataResponse, productDataTester.path, productDataTester.validTypes);
+
+            //getMediaContent test
+            var mediaContentTester = new ProductMediaClient();
+            var mediaContentResponse = mediaContentTester.getMediaContent();
+            printResponse(mediaContentResponse, mediaContentTester.path, mediaContentTester.validTypes);
+
+            Console.WriteLine("Finish response processing, files saved on " + Environment.ExpandEnvironmentVariables(Path.Combine(ConfigurationManager.AppSettings.Get("responseSavePath"))));
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();            
+        }
+
+        static void printResponse(object response, string path, List<Type> validTypes)
+        {
+            var responseType = response.GetType();
+            var output = new StringBuilder();
+            output.AppendLine("Response Object: " + responseType.Name);
+            HierarchyPrinter.validTypes = validTypes;
+            foreach (var property in responseType.GetProperties())
+            {
+                HierarchyPrinter.printMembersValues(property, property.GetValue(response), ref output, 1);
+            }
+            Console.Write(output.ToString());
+            TextWriter writer = new StreamWriter(path);
+            writer.Write(output.ToString());
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
